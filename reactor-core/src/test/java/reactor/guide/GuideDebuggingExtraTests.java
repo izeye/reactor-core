e3f4ce74ccc2faf85a16948c10782a3f1d0724b0
Simon Baslé <sbasle@pivotal.io>
2019-07-03 12:28:02 +0200

    fixed GuideDebuggingExtraTests

--- reactor-core/src/test/java/reactor/guide/GuideDebuggingExtraTests.java
+++ reactor-core/src/test/java/reactor/guide/GuideDebuggingExtraTests.java
@@ -47,12 +47,12 @@ public class GuideDebuggingExtraTests {
 
 			assertThat(debugStack)
 					.endsWith("Error has been observed at the following site(s):\n"
-							+ "\t|_       Flux.map ⇢ at reactor.guide.FakeRepository.findAllUserByName(FakeRepository.java:27)\n"
-							+ "\t|_       Flux.map ⇢ at reactor.guide.FakeRepository.findAllUserByName(FakeRepository.java:28)\n"
-							+ "\t|_    Flux.filter ⇢ at reactor.guide.FakeUtils1.lambda$static$1(FakeUtils1.java:29)\n"
-							+ "\t|_ Flux.transform ⇢ at reactor.guide.GuideDebuggingExtraTests.debuggingActivatedWithDeepTraceback(GuideDebuggingExtraTests.java:40)\n"
-							+ "\t|_   Flux.elapsed ⇢ at reactor.guide.FakeUtils2.lambda$static$0(FakeUtils2.java:30)\n"
-							+ "\t|_ Flux.transform ⇢ at reactor.guide.GuideDebuggingExtraTests.debuggingActivatedWithDeepTraceback(GuideDebuggingExtraTests.java:41)\n\n");
+							+ "\t|_        Flux.map ⇢ at reactor.guide.FakeRepository.findAllUserByName(FakeRepository.java:27)\n"
+							+ "\t|_        Flux.map ⇢ at reactor.guide.FakeRepository.findAllUserByName(FakeRepository.java:28)\n"
+							+ "\t|_     Flux.filter ⇢ at reactor.guide.FakeUtils1.lambda$static$1(FakeUtils1.java:29)\n"
+							+ "\t|_ Flux.composeNow ⇢ at reactor.guide.GuideDebuggingExtraTests.debuggingActivatedWithDeepTraceback(GuideDebuggingExtraTests.java:40)\n"
+							+ "\t|_    Flux.elapsed ⇢ at reactor.guide.FakeUtils2.lambda$static$0(FakeUtils2.java:30)\n"
+							+ "\t|_ Flux.composeNow ⇢ at reactor.guide.GuideDebuggingExtraTests.debuggingActivatedWithDeepTraceback(GuideDebuggingExtraTests.java:41)\n\n");
 		}
 		finally {
 			Hooks.resetOnOperatorDebug();
